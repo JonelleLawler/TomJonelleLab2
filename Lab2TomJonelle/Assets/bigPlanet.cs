7353d83fd61d24f3890733fba7c80140485737fb
Thomas Lloyd <c00231308@itcarlow.ie>
2019-09-30 16:15:28 +0100

    Added all the prefabs for all asteroids

---
+++ Lab2TomJonelle/Assets/bigPlanet.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bigPlanet : MonoBehaviour
+{
+    private float speed;
+    private float moveHorizontal;
+    private float moveVerticle;
+
+
+    private Rigidbody2D rb2d;
+    private int count;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        speed = Random.Range(0.1f, 0.5f);
+        moveHorizontal = Random.Range(-2, 2);
+        moveVerticle = Random.Range(-2, 2);
+
+    }
+
+    void FixedUpdate()
+    {
+
+        Vector2 movement = new Vector2(moveHorizontal, moveVerticle);
+        rb2d.AddForce(movement * speed);
+    }
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        /*
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            setCountText();
+        }
+        */
+    }
+}
